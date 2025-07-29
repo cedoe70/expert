b796d9e88e44f0000f3a9659fd6fd4d90a8eaf24
cedoe70 <cedoe70@gmail.com>
2025-07-29 08:19:39 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import InvestmentPlans from "./pages/admin/InvestmentPlans";
+import "./styles/globals.css";
 
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/admin/investment-plans" element={<InvestmentPlans />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
