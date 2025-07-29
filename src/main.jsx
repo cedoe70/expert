ad196e12d96b7de3205ad2b022b445f29a74ef32
cedoe70 <cedoe70@gmail.com>
2025-07-29 09:52:09 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./index.css";
+
+// Pages
+import AdminDashboard from "./pages/admin/AdminDashboard";
 import InvestmentPlans from "./pages/admin/InvestmentPlans";
-import "./styles/globals.css";
+
+// You can add the following as you build them:
+import KYCRequests from "./pages/admin/KYCRequests";
+import Withdrawals from "./pages/admin/Withdrawals";
+import UserBalances from "./pages/admin/UserBalances";
+import DepositAddresses from "./pages/admin/DepositAddresses";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/admin/investment-plans" element={<InvestmentPlans />} />
+        <Route path="/admin/kyc" element={<KYCRequests />} />
+        <Route path="/admin/withdrawals" element={<Withdrawals />} />
+        <Route path="/admin/users" element={<UserBalances />} />
+        <Route path="/admin/deposit-addresses" element={<DepositAddresses />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
