aeba675f455b7c485b4979f0b2a37d58e228154a
cedoe70 <cedoe70@gmail.com>
2025-07-29 13:28:33 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./index.css";
-
-// Admin pages
-import InvestmentPlans from "./pages/admin/InvestmentPlans";
-import KYCRequests from "./pages/admin/KYCRequests";
-import Withdrawals from "./pages/admin/Withdrawals";
-import UserBalances from "./pages/admin/UserBalances";
-import DepositAddresses from "./pages/admin/DepositAddresses";
+import App from "./App";
+import "./index.css"; // ✅ This matches the file path (in src/)
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/admin/investment-plans" element={<InvestmentPlans />} />
-        <Route path="/admin/kyc-requests" element={<KYCRequests />} />
-        <Route path="/admin/withdrawals" element={<Withdrawals />} />
-        <Route path="/admin/user-balances" element={<UserBalances />} />
-        <Route path="/admin/deposit-addresses" element={<DepositAddresses />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
