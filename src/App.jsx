db9ec8d4d6aae79b53e54d295b900833fec69b46
cedoe70 <cedoe70@gmail.com>
2025-07-30 06:23:11 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-// src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 // Admin Layout and Pages
 import AdminLayout from "./components/admin/AdminLayout";
-import Dashboard from "./pages/admin/Dashboard";
-import InvestmentPlans from "./pages/admin/InvestmentPlans";
-import KYCRequests from "./pages/admin/KYCRequests";
-import Withdrawals from "./pages/admin/Withdrawals";
-import UserBalances from "./pages/admin/UserBalances";
-import DepositAddresses from "./pages/admin/DepositAddresses";
-import ActivityLog from "./pages/admin/ActivityLog";
-import Settings from "./pages/admin/Settings";
+import AdminDashboard from "./components/admin/Dashboard";
+import InvestmentPlans from "./components/admin/InvestmentPlans";
+import KYCRequests from "./components/admin/KYCRequests";
+import Withdrawals from "./components/admin/Withdrawals";
+import UserBalances from "./components/admin/UserBalances";
+import DepositAddresses from "./components/admin/DepositAddresses";
+import ActivityLog from "./components/admin/ActivityLog";
+import Settings from "./components/admin/Settings";
 
 // User Layout and Pages
 import UserLayout from "./components/user/UserLayout";
-import UserDashboard from "./pages/user/Dashboard";
-// Future: import Invest from "./pages/user/Invest";
-// Future: import Withdraw from "./pages/user/Withdraw";
-// Future: import KYC from "./pages/user/KYC";
+import UserDashboard from "./components/user/Dashboard";
+import Invest from "./components/user/Invest";
+import Withdraw from "./components/user/Withdraw";
+import KYC from "./components/user/KYC";
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Redirect root to admin dashboard */}
-        <Route path="/" element={<Navigate to="/admin/dashboard" replace />} />
+        {/* Redirect root to admin investment plans page */}
+        <Route path="/" element={<Navigate to="/admin/investment-plans" replace />} />
 
-        {/* Admin Routes */}
-        <Route path="/admin/*" element={<AdminLayout />}>
-          <Route path="dashboard" element={<Dashboard />} />
+        {/* Admin routes */}
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route path="dashboard" element={<AdminDashboard />} />
           <Route path="investment-plans" element={<InvestmentPlans />} />
           <Route path="kyc-requests" element={<KYCRequests />} />
           <Route path="withdrawals" element={<Withdrawals />} />
@@ -39,13 +38,12 @@ function App() {
           <Route path="settings" element={<Settings />} />
         </Route>
 
-        {/* User Routes */}
-        <Route path="/user/*" element={<UserLayout />}>
+        {/* User routes */}
+        <Route path="/user" element={<UserLayout />}>
           <Route path="dashboard" element={<UserDashboard />} />
-          {/* Uncomment and add these as you build them:
           <Route path="invest" element={<Invest />} />
           <Route path="withdraw" element={<Withdraw />} />
-          <Route path="kyc" element={<KYC />} /> */}
+          <Route path="kyc" element={<KYC />} />
         </Route>
       </Routes>
     </Router>
